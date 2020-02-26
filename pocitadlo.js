e001438a041bb51e4131721b65255691d73a0123
datastory.cz <honza@datastory.cz>
2020-02-26 16:07:48 +0100

    pocitadlo pripadu

---
+++ pocitadlo.js
@@ -0,0 +1,67 @@
+function niceDate(val) {
+    const parts = val.split('/');
+    return `${parts[2]}. ${parts[1]}.`;
+}
+  
+  fetch('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/cases_time_v3/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Report_Date_String%20asc&resultOffset=0&resultRecordCount=2000&cacheHint=true')
+    .then((response) => response.json())
+    .then((data) => {
+      const days = [];
+      const cina = [];
+      const svet = [];
+      const vylec = [];
+  
+      data.features.forEach((ftr) => {
+        days.push(niceDate(ftr.attributes.Report_Date_String));
+        cina.push(ftr.attributes.Mainland_China);
+        svet.push(ftr.attributes.Other_Locations);
+        vylec.push(ftr.attributes.Total_Recovered);
+      });
+  
+      Highcharts.chart('corona_count', {
+        title: {
+          text: 'Počet případů koronaviru',
+        },
+        credits: {
+          enabled: false,
+        },
+        subtitle: {
+          text: 'Zdroj: <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports">WHO</a>, <a href="https://www.cdc.gov/coronavirus/2019-ncov/index.html">CDC</a>, <a href="https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases">ECDC</a>, <a href="http://www.nhc.gov.cn/xcs/yqtb/list_gzbd.shtml">NHC</a> a <a href="https://3g.dxy.cn/newh5/view/pneumonia?scene=2&amp;clicktime=1579582238&amp;enterid=1579582238&amp;from=singlemessage&amp;isappinstalled=0">DXY</a>.'
+        },
+        yAxis: {
+          title: {
+            text: 'případy',
+          },
+        },
+        xAxis: {
+          categories: days,
+        },
+        colors: ['#de2d26', '#756bb1', '#31a354'],
+        legend: {
+          layout: 'horizontal',
+          verticalAlign: 'bottom',
+        },
+        plotOptions: {
+          series: {
+            label: {
+              connectorAllowed: false,
+            },
+          },
+        },
+        tooltip: {
+          shared: true,
+          crosshairs: true,
+        },
+        series: [{
+          name: 'Nakažení v pevninské Číně',
+          data: cina,
+        }, {
+          name: 'Případy v ostatních zemích',
+          data: svet,
+        }, {
+          name: 'Celkem vyléčení',
+          data: vylec,
+        }],
+      });
+    });
+  
