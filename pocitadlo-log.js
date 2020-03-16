f0dff514967e98ba4c682e6bc8b2d8f28914b1a1
datastory.cz <honza@datastory.cz>
2020-03-16 10:56:40 +0100

    singapur do log grafu

--- pocitadlo-log.js
+++ pocitadlo-log.js
@@ -18,6 +18,7 @@
         "Republic of Korea": "Korejská republika",
         "Korea, South": "Jižní Korea",
         "Taiwan*": "Taiwan",
+        "Singapore": "Singapur",
     }
     
     fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
