c4fb7ddb05bee3d8e35707ad46fc56527c1c8d17
datastory.cz <honza@datastory.cz>
2020-03-18 12:44:54 +0100

    widget aktualniho stavu CR

---
+++ status-cz.js
@@ -0,0 +1,16 @@
+(function() {
+    fetch('https://api.apify.com/v2/key-value-stores/K373S4uCFR9W1K8ei/records/LATEST?disableRedirect=true')
+        .then((response) => response.json())
+        .then((data) => {
+            const dte = data.lastUpdatedAtSource.split('T')[0].split('-')
+            const hr = data.lastUpdatedAtSource.split('T')[1].split(':')
+            
+            document.getElementById('status_cz_testovani').innerText = data.totalTested
+            document.getElementById('status_cz_nakazeni').innerText = data.infected
+            document.getElementById('status_cz_vyleceni').innerText = data.recovered
+            document.getElementById('status_cz_update').innerText = `${parseInt(dte[2])}. ${parseInt(dte[1])}. v ${parseInt(hr[0]) + 1}:${hr[1]}`
+    })
+})()
+
+
+
