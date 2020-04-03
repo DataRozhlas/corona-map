1d5233a6abfd017d41a54bf42033c0d5a015ec81
datastory.cz <honza@datastory.cz>
2020-04-03 10:01:43 +0200

    čas

--- status-cz.js
+++ status-cz.js
@@ -2,8 +2,7 @@
     fetch('https://api.apify.com/v2/key-value-stores/K373S4uCFR9W1K8ei/records/LATEST?disableRedirect=true')
         .then((response) => response.json())
         .then((data) => {
-            const dte = data.lastUpdatedAtSource.split('T')[0].split('-')
-            const hr = data.lastUpdatedAtSource.split('T')[1].split(':')
+            const dte = new Date(data.lastUpdatedAtSource)
             
             if ( (document.getElementById('status_cz_testovani').innerText === '') | (parseInt(document.getElementById('status_cz_testovani').innerText) < data.totalTested) ) {
                 document.getElementById('status_cz_testovani').innerText = data.totalTested
@@ -20,7 +19,7 @@
             if ( (document.getElementById('status_cz_zemreli').innerText === '') | (parseInt(document.getElementById('status_cz_zemreli').innerText) < data.deceased) ) {
                 document.getElementById('status_cz_zemreli').innerText = data.deceased
             }
-            document.getElementById('status_cz_update').innerText = `${parseInt(dte[2])}. ${parseInt(dte[1])}. v ${parseInt(hr[0])}:${hr[1]}`
+            document.getElementById('status_cz_update').innerText = `${dte.getDate()}. ${dte.getUTCMonth() + 1}. v ${dte.getHours()}:${dte.getMinutes()}`
     })
 
     fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pub?gid=0&single=true&output=csv')
