1c32849e107d6dbf76782c8b33e8ceed1d4bb57d
datastory.cz <honza@datastory.cz>
2020-07-21 15:50:19 +0200

    nakazeni vsichni

--- nakazeni-stari.js
+++ nakazeni-stari.js
@@ -2,24 +2,37 @@
       fetch('https://data.irozhlas.cz/covid-uzis/osoby.json')
           .then((response) => response.json())
           .then((data) => {
-              console.log(data.data)
-
               const dt = []
+              const dtOld = []
               data.data.forEach(row => {
-                  if (parseInt(row.Vek) >= 65) { //rizikovi dle https://www.irozhlas.cz/zpravy-domov/statistika-dusek-uzis-covid-koronavir-index-mapa-semafor_2007031702_cib
+                const pDate = Date.parse(row.DatumHlaseni)
+                let rec = dt.find(e => e[0] === pDate)
+                if (rec !== undefined) {
+                    rec[1] += 1
+                } else {
+                    dt.push([pDate, 1])
+                }
+              })
+
+              data.data.forEach(row => {
+                if (parseInt(row.Vek) >= 65) { //rizikovi dle https://www.irozhlas.cz/zpravy-domov/statistika-dusek-uzis-covid-koronavir-index-mapa-semafor_2007031702_cib
                     const pDate = Date.parse(row.DatumHlaseni)
-                    let rec = dt.find(e => e[0] === pDate)
+                    let rec = dtOld.find(e => e[0] === pDate)
                     if (rec !== undefined) {
                         rec[1] += 1
                     } else {
-                        dt.push([pDate, 1])
+                        dtOld.push([pDate, 1])
                     }
-                  }
+                }
               })
 
               dt.sort( (a, b) => {
                   return a[0] - b[0]
               })
+
+              dtOld.sort( (a, b) => {
+                return a[0] - b[0]
+                })
          
               Highcharts.chart('corona_inf_old', {
                 chart: {
@@ -66,6 +79,11 @@
                     {
                       name: 'nakažení',
                       data: dt
+                    },
+                    {
+                        name: 'nakažení senioři',
+                        data: dtOld,
+                        color: 'blue'
                     }
                   ]
             })
