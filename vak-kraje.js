1be64b5c278151f4cf65289c20aa2554918b26ad
datastory <honza@datastory.cz>
2021-01-25 07:16:28 +0100

    wording vakciny

--- vak-kraje.js
+++ vak-kraje.js
@@ -54,7 +54,7 @@
         },
         colors: ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', 'lightgray', '#b15928', 'black', '#01665e', '#542788'],
         title: {
-          text: 'Celkem očkovaní v krajích',
+          text: 'Celkem podané vakcíny v krajích',
           align: 'left',
           style: {
             fontWeight: 'bold',
