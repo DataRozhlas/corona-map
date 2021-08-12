4e7a2c207798598939d9519d1d32300bd092b7f6
datastory <honza@datastory.cz>
2021-08-12 17:35:05 +0200

    testy a nakazeni update

--- cr-pct.js
+++ cr-pct.js
@@ -1,122 +1,121 @@
 (function () {
-  function unfuckUZISdate(fuckery) {
-    const shitcocks = fuckery.split('.');
-    return Date.parse(`${shitcocks[2]}-${shitcocks[1]}-${shitcocks[0]}`);
-  }
-
-  fetch('https://data.irozhlas.cz/covid-uzis/nakaza.json')
+  fetch('https://data.irozhlas.cz/covid-uzis/testy-pcr-antigenni.json')
     .then((response) => response.json())
-    .then((nakaza) => {
-      fetch('https://data.irozhlas.cz/covid-uzis/testy.json')
-        .then((response) => response.json())
-        .then((testy) => {
-          const srs = [];
-          let infected = nakaza.data.map((v) => [Date.parse(v.datum), parseInt(v.pocetCelkem)]);
+    .then((testy) => {
+      const srs = [];
+      let infected = testy.data.map((v) => [Date.parse(v.datum), parseInt(v.incidence_pozitivni)]);
 
-          let tested = testy
-            .data.map((v) => [unfuckUZISdate(v.datum), parseInt(v.kumulativni_pocet_testu)]);
+      let tested = testy
+        .data.map((v) => [
+          Date.parse(v.datum),
+          parseInt(v.pocet_PCR_testy, 10) + parseInt(v.pocet_AG_testy, 10),
+        ]);
 
-          infected = infected.filter((v) => v[0] > 1584316800000); // od 16. 3.
-          tested = tested.filter((v) => v[0] > 1584316800000);
+      infected = infected.filter((v) => v[0] > 1584316800000); // od 16. 3.
+      tested = tested.filter((v) => v[0] > 1584316800000);
 
-          const pctDiff = [];
-          infected.forEach((v, ind) => {
-            if (ind <= 1) { return; }
-            const diff = ((v[1] - infected[ind - 1][1]) / infected[ind - 1][1]) * 100;
-            if ((diff !== Infinity) && !(isNaN(diff))) {
-              pctDiff.push([v[0], diff]);
-            }
-          });
+      infected.sort((a, b) => a[0] - b[0]);
+      tested.sort((a, b) => a[0] - b[0]);
 
-          const pctTstDiff = [];
-          tested.forEach((v, ind) => {
-            if (ind <= 1) { return; }
-            const diff = ((v[1] - tested[ind - 1][1]) / tested[ind - 1][1]) * 100;
-            if ((diff !== Infinity) && !(isNaN(diff))) {
-              pctTstDiff.push([v[0], diff]);
-            }
-          });
 
-          srs.push(
-            {
-              name: 'Nárůst nakažených',
-              data: pctDiff,
-              color: '#de2d26',
-              visible: true,
-              marker: {
-                symbol: 'circle',
-              },
-            },
-          );
 
-          srs.push(
-            {
-              name: 'Přírůst testů',
-              data: pctTstDiff,
-              color: '#3182bd',
-              visible: true,
-              marker: {
-                symbol: 'circle',
-              },
-            },
-          );
+      const pctDiff = [];
+      infected.forEach((v, ind) => {
+        if (ind <= 1) { return; }
+        const diff = (v[1] / infected[ind - 1][1]) * 100;
+        if ((diff !== Infinity) && !(isNaN(diff))) {
+          pctDiff.push([v[0], diff]);
+        }
+      });
 
-          Highcharts.chart('corona_cr_pct', {
-            title: {
-              text: 'Procentuální nárůst testů a zjištěných nakažených COVID-19 v Česku (oproti předchozímu dni)',
-              useHTML: true,
+      const pctTstDiff = [];
+      tested.forEach((v, ind) => {
+        if (ind <= 1) { return; }
+        const diff = (v[1] / tested[ind - 1][1]) * 100;
+        if ((diff !== Infinity) && !(isNaN(diff))) {
+          pctTstDiff.push([v[0], diff]);
+        }
+      });
+
+      srs.push(
+        {
+          name: 'Nárůst nakažených',
+          data: pctDiff,
+          color: '#de2d26',
+          visible: true,
+          marker: {
+            symbol: 'circle',
+          },
+        },
+      );
+
+      srs.push(
+        {
+          name: 'Přírůst testů',
+          data: pctTstDiff,
+          color: '#3182bd',
+          visible: true,
+          marker: {
+            symbol: 'circle',
+          },
+        },
+      );
+
+      Highcharts.chart('corona_cr_pct', {
+        title: {
+          text: 'Procentuální nárůst testů a zjištěných nakažených COVID-19 v Česku (oproti předchozímu dni)',
+          useHTML: true,
+        },
+        chart: {
+          spacingLeft: 0,
+          spacingRight: 0,
+        },
+        credits: {
+          href: 'https://koronavirus.mzcr.cz/',
+          text: 'Zdroj dat: MZ ČR',
+        },
+        yAxis: {
+          title: {
+            text: 'nárůst v %',
+          },
+        },
+        xAxis: {
+          type: 'datetime',
+          endOnTick: true,
+          showLastLabel: true,
+          startOnTick: true,
+          labels: {
+            formatter() {
+              return Highcharts.dateFormat('%d. %m.', this.value);
             },
-            chart: {
-              spacingLeft: 0,
-              spacingRight: 0,
+          },
+        },
+        tooltip: {
+          formatter() {
+            return this.points.reduce((s, point) => `${s}<br/><span style="color: ${point.series.color};">${point.series.name}:${(Math.round(point.y * 10) / 10).toFixed(1)} %`, `<b>${Highcharts.dateFormat('%d. %m.', this.x)}</b>`);
+          },
+          shared: true,
+          useHTML: true,
+        },
+        legend: {
+          layout: 'horizontal',
+          align: 'center',
+          verticalAlign: 'bottom',
+        },
+        plotOptions: {
+          line: {
+            animation: false,
+            marker: {
+              enabled: false,
             },
-            credits: {
-              href: 'https://koronavirus.mzcr.cz/',
-              text: 'Zdroj dat: MZ ČR',
+          },
+          series: {
+            label: {
+              connectorAllowed: false,
             },
-            yAxis: {
-              title: {
-                text: 'nárůst v %',
-              },
-            },
-            xAxis: {
-              type: 'datetime',
-              endOnTick: true,
-              showLastLabel: true,
-              startOnTick: true,
-              labels: {
-                formatter() {
-                  return Highcharts.dateFormat('%d. %m.', this.value);
-                },
-              },
-            },
-            tooltip: {
-              formatter() {
-                return this.points.reduce((s, point) => `${s}<br/><span style="color: ${point.series.color};">${point.series.name}:${(Math.round(point.y * 10) / 10).toFixed(1)} %`, `<b>${Highcharts.dateFormat('%d. %m.', this.x)}</b>`);
-              },
-              shared: true,
-              useHTML: true,
-            },
-            legend: {
-              layout: 'horizontal',
-              align: 'center',
-              verticalAlign: 'bottom',
-            },
-            plotOptions: {
-              line: {
-                animation: false,
-                marker: {
-                  enabled: false,
-                },
-              },
-              series: {
-                label: {
-                  connectorAllowed: false,
-                },
-              },
-            },
-            series: srs,
-          });
-        });
+          },
+        },
+        series: srs,
+      });
     });
 }());
